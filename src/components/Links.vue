15ccbaaf1107da0f93d638afd578cc624f355aa3
Styl <kostasstilianou@gmail.com>
2024-12-24 09:09:22 +0200

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -8,7 +8,7 @@ const linkList = ref<any[]>([{href:`https://www.linkedin.com/in/konstantinos-sty
 
 <template>
     <Grid columns="5" class="justify-items-center grid-flow-row gap-x-1 max-sm:gap-x-8">
-      <a v-for="(link,index) in linkList" :key="index" class="block p-1 bg-neutral-100 bg-opacity-10 rounded-xl hover:scale-110 hover:bg-white hover:text-black transition duration-500 ease-linear" :href="link.href" target="_blank">
+      <a v-for="(link,index) in linkList" :key="index" class="block p-1 bg-neutral-100 bg-opacity-10 rounded-xl hover:scale-110 hover:bg-white hover:text-black transition duration-500 ease-linear" :href="link.href" target="_blank" aria-label="Social_Links">
         <Icon width="24px"  :icon="link.icon"/><span class="hidden">‎</span>
       </a>
       <a href="mailto:stylsbot@gmail.com" class="block p-1 bg-neutral-100 bg-opacity-10 rounded-xl hover:scale-110 hover:bg-white hover:text-black transition duration-500 ease-linear"  target="_blank">
